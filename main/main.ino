809802adf23749dbfb408a213aff5d4337328e07
Vikas Gaikwad <vksgaikwad3@gmail.com>
2016-11-22 21:31:52 +0530

    updated main files

---
+++ main/main.ino
@@ -0,0 +1,222 @@
+/* Description : Read all the Data from MODBUS Slaves.
+   Hardware Connections : Arduino MEGA SERIAL2(Hardware Serial) 
+                          RX2 -----> R0 (MAX485 Chip Adapter)
+                          TX2 -----> DI (MAX485 Chip Adapter)
+                          D2  -----> DE/RE (MAX485 Chip Adapter)
+   MODBUS  Device Settings : 
+
+   SLAVE 1 : Milk Chiller Panel 
+   Slave ID : 1, Baud Rate : 9600, Parity : NONE, Stop bit : 1
+
+   SLAVE 2 : Energy Meter
+   Slave ID : 2, Baud Rate: 9600, Parity :None, Stop bit : 1
+ * 
+ * MODBUS Electrical Connections: 
+ * 1> MILK Chiller Serial(RS 232) --> 485 Converter Pin 1 --> A of MAX485 Chip Adapter.
+ *                                    485 Converter Pin 2 --> B of MAX485 Chip Adapter.        
+ * 2> Energy Meter (RS485 Signal) --> A (Green Wire) -->  B of MAX485 Chip Adapter. 
+ *                                    B (Yellow Wire) --> A of MAX485 Chip Adapter.
+ *   
+ *   
+ */
+
+
+#include <SimpleModbusMaster.h>
+#include "sim900.h"     
+#include "serverConfg.h"          
+
+sim900_GPRS myGateway;    // Gateway object
+
+#define baud 9600
+#define timeout 1000
+#define polling 200
+#define retry_count 20
+
+#define TxEnablePin 2 
+
+#define LED 13
+
+#define TOTAL_NO_OF_REGISTERS 33        // Control Panel Resisters = 17 , Energy Meter Resisters = 16
+
+enum
+{
+  PACKET1,
+  PACKET2,
+  PACKET3,
+  PACKET4,
+  PACKET5,
+  PACKET6,
+  PACKET7,
+  PACKET8,
+  PACKET9,
+  TOTAL_NO_OF_PACKETS // leave this last entry
+};
+
+Packet packets[TOTAL_NO_OF_PACKETS];
+
+int regs[TOTAL_NO_OF_REGISTERS];     // All the Modbus Resisters gets stored here in buffer.
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  //READ_INPUT_REGISTERS
+  //READ_HOLDING_REGISTERS
+   //MILK CHILLER PACKET 
+   modbus_construct(&packets[PACKET1], 1, READ_HOLDING_REGISTERS, 4, 17, 0);  // Control Panel Resisters [40005 - 40021]
+  
+   // ENERGY METER PACKET
+    
+    modbus_construct(&packets[PACKET2], 2,READ_INPUT_REGISTERS , 0, 2, 17);    //30001-2 Line Voltage reg[17-18]       HB :17278 LB :44628
+    modbus_construct(&packets[PACKET3], 2,READ_INPUT_REGISTERS , 6, 2, 19);    // 30007-8 Line Current reg[19-20]      
+    modbus_construct(&packets[PACKET4], 2,READ_INPUT_REGISTERS , 42,2, 21);    // 30043-44 Device voltage reg[21-22]
+    modbus_construct(&packets[PACKET5], 2,READ_INPUT_REGISTERS , 46,2, 23);    // 30047-48 Device Current reg[23-24]
+    modbus_construct(&packets[PACKET6], 2,READ_INPUT_REGISTERS , 72,4, 25);    // 30073-74 Power Consumption  reg[25-26]   Reading 16384(73)[IMP]
+
+    modbus_construct(&packets[PACKET7], 2,READ_INPUT_REGISTERS , 86,2, 27);    // 30089-90 Sys Power(W) reg[27-28]
+    modbus_construct(&packets[PACKET8], 2,READ_INPUT_REGISTERS , 226,2,29);    // 30227-28 Device Run Hr reg[29-30]     
+    modbus_construct(&packets[PACKET9], 2,READ_INPUT_REGISTERS , 228,2,31);    // 30227-28 Power Available Time reg[31-32]    
+    
+   
+  //modbus_construct(&packets[PACKET2], 1, PRESET_MULTIPLE_REGISTERS, 0, 10, 0);
+  
+  modbus_configure(&Serial2, baud, SERIAL_8N1, timeout, polling, retry_count, TxEnablePin, packets, TOTAL_NO_OF_PACKETS, regs);
+  delay(100);
+  pinMode(LED, OUTPUT);
+
+  // Setup GSM Module 
+  Serial1.begin(9600);   // To connect SIM900A and send AT Commands
+  myGateway.power_on();            // POWER ON GSM Module for communication 
+
+  
+}
+
+void loop()
+{
+  for(int i=0;i<200;i++)
+  {
+    modbus_update();
+    delay(10);Serial.println(i);
+  }
+ 
+  volatile float battery_Temp = regs[0]/10.0;
+  volatile float milk_Temp = regs[1]/10.0;
+  volatile float auxillary_Temp = regs[2]/10.0;
+  volatile uint8_t battery_Volt = regs[3];
+  volatile uint8_t ac_Volt = regs[4];
+  volatile float compressor_Current = regs[5]/10.0;
+  volatile float pump_Current = regs[6]/10.0;
+  volatile uint8_t charg_pump_Relay = regs[7];
+  volatile uint8_t condensor_Relay = regs[8];
+  volatile uint8_t compressor_Relay = regs[9];
+  volatile uint8_t inverter_Relay = regs[10];
+  volatile uint8_t agitator_Relay = regs[11];
+  volatile uint8_t tank_Relay = regs[12];
+  volatile uint8_t shiva_Relay = regs[13];
+  volatile uint8_t discharge_pump_Relay = regs[14];
+  volatile uint32_t compressor_run_Hour = regs[16];
+     
+  Serial.print("Battery Temp.: ");Serial.println(battery_Temp,DEC);
+  Serial.print("Milk Temp.: ");Serial.println(milk_Temp,DEC);
+  Serial.print("Auxillary Temp.: ");Serial.println(auxillary_Temp,DEC);
+  Serial.print("Battery Volt.: ");Serial.println(battery_Volt,DEC);
+  Serial.print("AC Voltage.: ");Serial.println(ac_Volt,DEC);
+  Serial.print("Copmressor Curr.: ");Serial.println(compressor_Current,DEC);
+  Serial.print("Pump Current.: ");Serial.println(pump_Current,DEC);
+  Serial.print("Ch Pump Relay.: ");Serial.println(charg_pump_Relay,DEC);
+  Serial.print("Condensor Relay.: ");Serial.println(condensor_Relay,DEC);
+  Serial.print("Compressor Relay.: ");Serial.println(compressor_Relay,DEC);
+  Serial.print("Inverter Relay.: ");Serial.println(inverter_Relay,DEC);
+  Serial.print("Agitator Relay.: ");Serial.println(agitator_Relay,DEC);
+  Serial.print("Tank Relay.: ");Serial.println(tank_Relay,DEC);
+  Serial.print("Shiva Relay.: ");Serial.println(shiva_Relay,DEC);
+  //Serial.print("Discharge Pump Relay.: ");Serial.println(regs[14],DEC);
+  Serial.print("Discharge Pump Relay.: ");Serial.println(discharge_pump_Relay,DEC);
+  Serial.print("Compressor Run Hr.: ");Serial.println(compressor_run_Hour,DEC);
+  delay(20);
+ 
+    // Energy Meter Readings 
+  Serial.println("************ Energy Meter Readings ************* ");
+  /*
+  Serial.print("Line Voltages[HB].: ");Serial.println(regs[17],HEX);
+  Serial.print("Line Voltages[LB].: ");Serial.println(regs[18],HEX);
+  Serial.print("Line Current [HB].: ");Serial.println(regs[19],HEX);
+  Serial.print("Line Current [LB].: ");Serial.println(regs[20],HEX);
+  Serial.print("Device Voltage [HB].: ");Serial.println(regs[21],HEX);
+  Serial.print("DEvice Voltage [LB].: ");Serial.println(regs[22],HEX);
+  Serial.print("Device Current [HB].: ");Serial.println(regs[23],HEX);
+  Serial.print("DEvice Current [LB].: ");Serial.println(regs[24],HEX);
+  Serial.print("POWER[Sys W] [HB].: ");Serial.println(regs[25],HEX);
+  Serial.print("POWER[Sys W] [LB].: ");Serial.println(regs[26],HEX);
+  Serial.print("Power Consumption [HB].: ");Serial.println(regs[27],HEX);
+  Serial.print("Power Consumption [LB].: ");Serial.println(regs[28],HEX);
+  Serial.print("Device Run Hr [HB].: ");Serial.println(regs[29],HEX);
+  Serial.print("DEvice Run Hr [LB].: ");Serial.println(regs[30],HEX);
+  Serial.print("Power Available Time [HB].: ");Serial.println(regs[31],HEX);
+  Serial.print("Power Available Time [LB].: ");Serial.println(regs[32],HEX);
+  */
+  delay(50);
+  float lineVolts = myGateway.hextofloat(regs[17],regs[18]);
+  float lineCurrent = myGateway.hextofloat(regs[19],regs[20]);
+  float deviceVolts = myGateway.hextofloat(regs[21],regs[22]);
+  float deviceCurrent = myGateway.hextofloat(regs[23],regs[24]);
+  float power = myGateway.hextofloat(regs[25],regs[26]);
+  float powerConsump = myGateway.hextofloat(regs[27],regs[28]);
+  float deviceRunHr = myGateway.hextofloat(regs[29],regs[30]);
+  float powerAvailable = myGateway.hextofloat(regs[31],regs[32]);
+  
+  float emarray[] = {lineVolts,lineCurrent,deviceVolts,deviceCurrent,power,powerConsump/1000,deviceRunHr,powerAvailable };
+  for(int i=0;i<8;i++)
+  {
+    Serial.println(emarray[i]);
+    delay(1);
+    
+  }
+  
+  delay(5000); 
+  /******************** Milk Chiller Temp Data Uploading *****************************/
+  int stime = millis();
+  myGateway.updateThinkSpeak(channel_apiKey[0],1,2,3,4,battery_Temp,milk_Temp,auxillary_Temp,battery_Volt);    //update Milk Chiller Channel 1 - 4 fields
+  Serial.print("Chiller 1-4 Time Taken :");Serial.println(millis() - stime );
+  delay(200);
+  int s1time = millis();
+  myGateway.updateThinkSpeak(channel_apiKey[0],5,6,7,8,ac_Volt,compressor_Current,pump_Current,compressor_run_Hour);    //update Milk Chiller Channel field 5 -8
+  Serial.print("Chiller 5 -8 Time Taken :");Serial.println(millis() - s1time );
+  delay(200);
+
+ /****************** Milk Chiller Relays Data Uploading ***************************/ 
+  int s2time = millis();
+  //myGateway.updateMilkChillerRelays(channel_apiKey[1],charg_pump_Relay,condensor_Relay,compressor_Relay,inverter_Relay,agitator_Relay,tank_Relay,shiva_Relay,discharge_pump_Relay );   // pass the parameters of milkChiller Relay fields 1 - 4
+  myGateway.updateThinkSpeak(channel_apiKey[1],1,2,3,4,charg_pump_Relay,condensor_Relay,compressor_Relay,inverter_Relay);    //update Milk Chiller Relays Channel field 1 -4
+  Serial.print("Chiller Relays 1-8 Time Taken :");Serial.println(millis() - s2time );
+  delay(200);
+ 
+  int s3time = millis();
+  myGateway.updateThinkSpeak(channel_apiKey[1],5,6,7,8,agitator_Relay,tank_Relay,shiva_Relay,discharge_pump_Relay);    //update Milk Chiller Relays Channel field 5 - 8
+  Serial.print("Chiller Relays 1-8 Time Taken :");Serial.println(millis() - s3time );
+  delay(200);
+  
+/*********************** Energy Meter Uploaing ****************************************/  
+  int s4time = millis();
+  myGateway.updateThinkSpeak(channel_apiKey[2],1,2,3,4,lineVolts,lineCurrent,deviceVolts,deviceCurrent);                  //update Energy meter 1 - 4 fields
+  Serial.print("Energy Meter 1-4 Time Taken :");Serial.println(millis() - s4time );
+  delay(200);
+  
+  int s5time = millis();
+  myGateway.updateThinkSpeak(channel_apiKey[2],5,6,7,8,power,powerConsump/1000,deviceRunHr,powerAvailable);               // Energy Meter 5 - 8 fields
+  Serial.print("Energy Meter 5 - 8 Time Taken :");Serial.println(millis() - s5time );
+  
+  /*myGateway.updateThinkSpeak(channel_apiKey[2],1,2,3,4,lineVolts,lineCurrent,deviceVolts,deviceCurrent);                  //update Energy meter 1 - 4 fields  
+    myGateway.updateThinkSpeak(channel_apiKey[0],1,2,3,4,100.79,20.70,30.20,40.40);
+    delay(1000);
+    myGateway.updateThinkSpeak(channel_apiKey[1],);                  //update Energy meter 1 - 4 fields  
+    delay(500);
+    
+   //myGateway.updateThinkSpeak(channel_apiKey[2],5,6,7,8,power,powerConsump/1000,deviceRunHr,powerAvailable);                    //update Energy meter 5 - 8 fields
+  //myGateway.updateThinkSpeak(channel_apiKey[2],5,6,7,8,41.1,912.24,901.14,9999);                                                //update Energy meter 5 - 8 fields
+    */
+  Serial.println("**** done ****");
+  delay(200);
+ 
+}
+
---
+++ main/sim900.cpp
@@ -0,0 +1,406 @@
+#include "sim900.h"
+
+
+// GSM/GPRS Configurations and Think Spwak Cloud Configurations Settings
+
+char aux_str[100];
+    
+char pin[]="";
+//char apn[]="imis/internet";     //idea
+char apn[]="airtelgprs.com";     //Airtel
+
+char user_name[]="";
+char password[]="";
+
+String getStr="";
+
+char IP_address[]="184.106.153.149";   //Think Speak Server IP Address
+char port[]= "80";                      // PORT Connected on 
+
+boolean sim900Status = false;
+
+/*************************************************************************************************/
+
+sim900_GPRS::sim900_GPRS()
+{
+    //power_on();     // GSM ON
+    
+}
+
+/*************************************************************************************************/
+
+void sim900_GPRS::power_on()
+{
+   uint8_t answer=0;
+    
+   sim900Status=true;
+   int ctT=0;
+ digitalWrite(13,HIGH);
+ delay(1000);
+ digitalWrite(13,LOW);
+ delay(2000);
+ digitalWrite(13,HIGH);
+ delay(5000);
+    // checks if the module is started
+    answer = sendATcommand("AT", "OK", 2000);
+    if (answer == 0)
+    {
+        // waits for an answer from the module
+        while(answer == 0){     // Send AT every two seconds and wait for the answer
+            answer = sendATcommand("AT", "OK", 2000);    
+      ctT++;
+    if(ctT==10){
+     sim900Status=true;
+     Serial.println("GSM OFF");
+    power_on();
+    }  
+      }
+    }
+    Serial.println("GSM ON Sucessfully");
+    // Once GSM get ON dothe folowing configurations to Send Systems Setting over SMS
+      
+    //sendATcommand("ATE0", "OK", 1000);        // ON/OFF AT command info
+   // sendATcommand("AT+CMGF=1", "OK",1000);    // Select SMS message Format 0 = PDU mode , 1 = Text Mode
+   // sendATcommand("AT+CMGD=1", "OK", 1000);   // Delete an SMS message
+   // sendATcommand("AT+CNMI=2,2,0,0,0\r", "OK", 1000);  //New SMS Indications 
+   // sendATcommand("AT+CMGR=1", "OK", 1000);            // Read SMS message has just arrived
+    
+    
+   // sendATcommand("AT+CCLK?", "OK", 1000);          // GSM Clock optional
+  //   sendATcommand("AT+CCLK=\"16/07/27,14:32:30+05\"", "OK", 1000);
+  //  sendSMS("hello");
+}
+
+/******************************************************************************************************************************************/
+
+int8_t sim900_GPRS::sendATcommand(char* ATcommand, char* expected_answer, unsigned int timeout)
+{
+
+    uint8_t x=0,  answer=0;
+    char response[100];         //Response buffer
+    unsigned long previous;
+
+    memset(response, '\0', 100);    // Initialize the string
+
+    delay(100);
+
+    while( Serial.available() > 0) Serial.read();    // Clean the input buffer
+
+    Serial.println(ATcommand);    // Send the AT command 
+    Serial1.println(ATcommand); //Response from Serial1 
+
+    x = 0;
+    previous = millis();
+
+    // this loop waits for the answer
+    do{
+        if(Serial1.available() != 0){    
+            // if there are data in the UART input buffer, reads it and checks for the asnwer
+            response[x] = Serial1.read();     //Read Response from Serial1 port
+            Serial.print(response[x]);        //Print response on Serial 0 port
+            x++;
+            // check if the desired answer  is in the response of the module
+            if (strstr(response, expected_answer) != NULL)    
+            {
+                answer = 1;
+            }
+        }
+    }
+    // Waits for the asnwer with time out
+    while((answer == 0) && ((millis() - previous) < timeout));    //Check till answer = 0 and timout period(ms)
+
+        return answer;
+}
+
+/**********************************************************************************************************************************************************/
+
+int8_t sim900_GPRS::sendATcommand2(String ATcommand, char* expected_answer1,char* expected_answer2, unsigned int timeout)
+{
+  
+    uint8_t x=0,  answer=0;
+   char response[100];
+    unsigned long previous;
+
+    memset(response, '\0', 100);    // Initialize the string
+
+    delay(100);
+
+    while( Serial1.available() > 0) Serial1.read();    // Clean the input buffer
+
+  Serial1.println(ATcommand);    // Send the AT command 
+Serial.println(ATcommand);
+    x = 0;
+    previous = millis();
+
+    // this loop waits for the answer
+    do{
+        // if there are data in the UART input buffer, reads it and checks for the asnwer
+        if(Serial1.available() != 0){    
+            response[x] = Serial1.read();
+             Serial.print(response[x]);
+            x++;
+            // check if the desired answer 1  is in the response of the module
+            if (strstr(response, expected_answer1) != NULL)    
+            {
+                answer = 1;
+            }
+            // check if the desired answer 2 is in the response of the module
+            else if (strstr(response, expected_answer2) != NULL)    
+            {
+                answer = 2;
+            }
+          
+         
+        }
+    }
+    // Waits for the asnwer with time out
+    while((answer == 0) && ((millis() - previous) < timeout));    
+
+    return answer;
+}
+
+/**************************************************************************************************************************************************************/
+
+int8_t sim900_GPRS::sendATcommand3(String ATcommand, char* expected_answer1,char* expected_answer2,char*expected_answer3, unsigned int timeout)
+{
+
+    uint8_t x=0,  answer=0;
+    char response[100];
+    unsigned long previous;
+
+    memset(response, '\0', 100);    // Initialize the string
+
+    delay(100);
+
+    while( Serial1.available() > 0) Serial1.read();    // Clean the input buffer
+
+    Serial1.println(ATcommand);    // Send the AT command 
+    Serial.println(ATcommand);
+    x = 0;
+    previous = millis();
+
+    // this loop waits for the answer
+    do{
+        // if there are data in the UART input buffer, reads it and checks for the asnwer
+        if(Serial1.available() != 0){    
+            response[x] = Serial1.read();
+             Serial.print(response[x]);
+            x++;
+            // check if the desired answer 1  is in the response of the module
+            if (strstr(response, expected_answer1) != NULL)    
+            {
+                answer = 1;
+            }
+            // check if the desired answer 2 is in the response of the module
+            if (strstr(response, expected_answer2) != NULL)    
+            {
+                answer = 2;
+            }
+            else if (strstr(response, expected_answer3) != NULL)    
+            {
+                answer = 3;
+            }
+          
+         
+        }
+    }
+    // Waits for the asnwer with time out
+    while((answer == 0) && ((millis() - previous) < timeout));    
+
+    return answer;
+}
+
+/*****************************************************************************************************************************************************/
+
+void sim900_GPRS::updateThinkSpeak(String channel_apiKey,uint8_t id1,uint8_t id2,uint8_t id3,uint8_t id4,float field1=0,float field2=0,float field3=0,float field4=0)  // ,float field5=0,float field6=0,float field7=0,float field8=0)
+{
+  
+   if(sim900Status==true)
+   {  // Selects Single-connection mode
+      if (sendATcommand2("AT+CIPMUX=0", "OK","ERROR", 1000) == 1)      // CIMPUX=0 is already set in Single-connection mode
+       {
+        // Waits for status IP INITIAL
+          while(sendATcommand("AT+CIPSTATUS","INITIAL", 1000)  == 0 );  // Check Current Connection Status
+          delay(5000);                                                   //wait 5 sec
+        //aux_str="AT+CSTT=\"airtelgprs.com\",\"\",\"\"";
+        //sendATcommand2(aux_str, "CONNECT OK", "CONNECT FAIL", 30000);
+          snprintf(aux_str, sizeof(aux_str), "AT+CSTT=\"%s\",\"%s\",\"%s\"", apn, user_name, password);  //Put GPRS setings
+        // Sets the APN, user name and password
+        if (sendATcommand2(aux_str, "OK",  "ERROR", 30000) == 1)
+        {            
+          // Waits for status IP START
+          while(sendATcommand("AT+CIPSTATUS", "START", 500)  == 0 );
+          delay(2000);
+          // Brings Up Wireless Connection
+          if (sendATcommand2("AT+CIICR", "OK", "ERROR", 30000) == 1)
+          {    
+              //delay(2000);
+              Serial.println("\n Bringup Wireless Connection ...........");
+             // Waits for status IP GPRSACT
+             while(sendATcommand("AT+CIPSTATUS", "GPRSACT", 1000)  == 0 );
+             delay(2000);
+            
+              // Gets Local IP Address
+              if (sendATcommand2("AT+CIFSR", ".", "ERROR", 10000) == 1)
+              {
+                 // Waits for status IP STATUS
+                 while(sendATcommand("AT+CIPSTATUS", "IP STATUS", 500)  == 0 );
+                 delay(2000);
+                 Serial.println("Opening TCP");
+                 //  aux_str="AT+CIPSTART=\"TCP\",\"184.106.153.149\",\"80\"";
+                 //  sendATcommand2(aux_str, "CONNECT OK", "CONNECT FAIL", 30000);
+                 snprintf(aux_str, sizeof(aux_str), "AT+CIPSTART=\"TCP\",\"%s\",\"%s\"", IP_address, port);
+                 // snprintf(aux_str, sizeof(aux_str), "AT+CIPSTART=\"TCP\",\"%s\",\"%s\"", thethingsiOserver,port);  // thethings.iO Server   
+                // Opens a TCP socket
+                 if (sendATcommand2(aux_str, "CONNECT OK", "CONNECT FAIL", 30000) == 1)
+                 {
+                         Serial.println("Connected");
+
+                         Serial.println("\n Send some data to TCP Socket............");
+                         getStr="GET /update?api_key="+ channel_apiKey +"&field"+ id1 +"=" +field1 + "&field"+ id2 + "=" +field2 +"&field" +id3 + "=" +field3 +"&field" +id4 +"=" +field4 
+                                                      /*+"&field5="+ field5  +"&field6="+ field6 +"&field7="+ field7 +"&field8=" + field8*/ + "\r\n\r\n";      //TCP packet to send GET Request on https (Think Speak)
+                          
+                          String sendcmd = "AT+CIPSEND="+ String(getStr.length());
+                          if (sendATcommand2(sendcmd, ">", "ERROR", 10000) == 1)    
+                            {
+                              delay(100);
+                              //ThingSpeak.writeField(Channel_ID, 1, field1, myWriteAPIKey);
+                              sendATcommand2(getStr, "SEND OK", "ERROR", 10000);      //Field1 Data sending from here
+                              delay(100);
+                            }
+                            Serial.println("Closing the Socket............");
+                            
+                            
+                             // Closes the socket
+                             sendATcommand2("AT+CIPCLOSE", "CLOSE OK", "ERROR", 20000);
+                             Serial.println("Conneection CLOSED OK");
+                  }
+                  else
+                  {
+                      Serial.println("Error opening the connection");
+                       
+                  }  
+              }
+          }
+        }
+       }
+   }
+    Serial.println("Shutting down the connection.........");
+    sendATcommand2("AT+CIPSHUT", "OK", "ERROR", 10000);
+    delay(5000);
+}
+
+
+/**********************************************************************************************************************************************************/
+void sim900_GPRS::updateMilkChillerRelays(String channel_apiKey,uint8_t field1,uint8_t field2,uint8_t field3,uint8_t field4,uint8_t field5,uint8_t field6,uint8_t field7,uint8_t field8)
+{
+   if(sim900Status==true)
+   {  // Selects Single-connection mode
+      if (sendATcommand2("AT+CIPMUX=0", "OK","ERROR", 1000) == 1)      // CIMPUX=0 is already set in Single-connection mode
+       {
+        // Waits for status IP INITIAL
+          while(sendATcommand("AT+CIPSTATUS","INITIAL", 1000)  == 0 );  // Check Current Connection Status
+          delay(5000);                                                   //wait 5 sec
+        //aux_str="AT+CSTT=\"airtelgprs.com\",\"\",\"\"";
+        //sendATcommand2(aux_str, "CONNECT OK", "CONNECT FAIL", 30000);
+          snprintf(aux_str, sizeof(aux_str), "AT+CSTT=\"%s\",\"%s\",\"%s\"", apn, user_name, password);  //Put GPRS setings
+        // Sets the APN, user name and password
+        if (sendATcommand2(aux_str, "OK",  "ERROR", 30000) == 1)
+        {            
+          // Waits for status IP START
+          while(sendATcommand("AT+CIPSTATUS", "START", 500)  == 0 );
+          delay(2000);
+          // Brings Up Wireless Connection
+          if (sendATcommand2("AT+CIICR", "OK", "ERROR", 30000) == 1)
+          {    
+              //delay(2000);
+              Serial.println("\n Bringup Wireless Connection ...........");
+             // Waits for status IP GPRSACT
+             while(sendATcommand("AT+CIPSTATUS", "GPRSACT", 1000)  == 0 );
+             delay(2000);
+            
+              // Gets Local IP Address
+              if (sendATcommand2("AT+CIFSR", ".", "ERROR", 10000) == 1)
+              {
+                 // Waits for status IP STATUS
+                 while(sendATcommand("AT+CIPSTATUS", "IP STATUS", 500)  == 0 );
+                 delay(2000);
+                 Serial.println("Opening TCP");
+                 //  aux_str="AT+CIPSTART=\"TCP\",\"184.106.153.149\",\"80\"";
+                 //  sendATcommand2(aux_str, "CONNECT OK", "CONNECT FAIL", 30000);
+                 snprintf(aux_str, sizeof(aux_str), "AT+CIPSTART=\"TCP\",\"%s\",\"%s\"", IP_address, port);
+                 // snprintf(aux_str, sizeof(aux_str), "AT+CIPSTART=\"TCP\",\"%s\",\"%s\"", thethingsiOserver,port);  // thethings.iO Server   
+                // Opens a TCP socket
+                 if (sendATcommand2(aux_str, "CONNECT OK", "CONNECT FAIL", 30000) == 1)
+                 {
+                         Serial.println("Connected");
+
+                         Serial.println("\n Send some data to TCP Socket............");
+                         getStr="GET /update?api_key="+ channel_apiKey +"&field1="+ field1 + "&field2="+ field2 +"&field3=" +field3 +"&field4=" +field4 
+                                                      +"&field5="+ field5  +"&field6="+ field6 +"&field7="+ field7 +"&field8=" + field8 + "\r\n\r\n";      //TCP packet to send GET Request on https (Think Speak)
+                          
+                          // getStr= "GET /raspberry/api.php?serviceName=test_api&field1=123\r\n\r\n";  //+ "\r\n\r\n";   //Smartnub packet request
+                          //String value = "{"values": [ { "key": "fun","value": "5000" } ] }\";
+                          //getStr="POST /v2/things/{{doAgkdNly8o29pusozJ-3mNnqET6dKKvcI4DmLVe-t4}}\" + value + "\r\n\r\n";   //thethingsIO request to write
+                          
+                          //getStr="PUT https://api.thingspeak.com/channels/"+ Channel_ID ;
+                         // getStr += apiKey;
+                         // getStr +="&field1=";
+                         // getStr += field1;
+                         // getStr += "\r\n\r\n";
+                          
+                          String sendcmd = "AT+CIPSEND="+ String(getStr.length());
+                          if (sendATcommand2(sendcmd, ">", "ERROR", 10000) == 1)    
+                            {
+                              delay(100);
+                              //ThingSpeak.writeField(Channel_ID, 1, field1, myWriteAPIKey);
+                              sendATcommand2(getStr, "SEND OK", "ERROR", 10000);      //Field1 Data sending from here
+                              delay(100);
+                            }
+                            Serial.println("Closing the Socket............");
+                            
+                            
+                             // Closes the socket
+                             sendATcommand2("AT+CIPCLOSE", "CLOSE OK", "ERROR", 20000);
+                             Serial.println("Conneection CLOSED OK");
+                  }
+                  else
+                  {
+                      Serial.println("Error opening the connection");
+                       
+                  }  
+              }
+          }
+        }
+       }
+   }
+    Serial.println("Shutting down the connection.........");
+    sendATcommand2("AT+CIPSHUT", "OK", "ERROR", 10000);
+    delay(5000);
+}
+
+
+/**********************************************************************************************************************************************************/
+
+float sim900_GPRS:: hextofloat(uint16_t hByte, uint16_t lByte)
+{
+  uint32_t hb = 0x0000FFFF & hByte ; 
+  uint32_t num = (hb << 16) | lByte ;
+  
+   //uint32_t lb = Serial.println(44628);
+  
+  uint32_t myString[1] ={num } ;
+  uint32_t val;
+  float floatVal;
+  
+  //Serial.print("Masking No :");Serial.println(hb);
+  //Serial.print("After Shift << 16: ");Serial.println(num);
+  num = myString[0];
+  //Serial.println(num);
+  floatVal = *((float*)&num);            // OR floatVal = *((float*)&myString);
+  //Serial.println(floatVal);
+
+  return floatVal; 
+}
+
+
