20969e0e389904133f21470cc071e3f42ffd6589
Vikas Gaikwad <vksgaikwad3@gmail.com>
2016-11-28 18:56:11 +0530

    updated server parameters

--- main/serverConfg.h
+++ main/serverConfg.h
@@ -15,14 +15,14 @@
     #define milkChillerRelayKey   "5CFI74KGNN3Z3CR1"        //api Key for Milk Chiller Relay Channel
     #define energyMeterKey        "DIU0GB1C3XJQTOFY"        //api Key for Energy Meter Channel
     
-    const int Channel_ID1 =  176340;                       //Milk Chiller ID 
+    //const int Channel_ID1 =  176340;                       //Milk Chiller ID 
     //const char * myWriteAPIKey = "XTUFVMDZYZLA0TB6";
     
     String channel_apiKey[] ={milkChillerKey, milkChillerRelayKey, energyMeterKey };                    //write API key
     
     // Milk Chiller Relays Channel
     
-    const int Channel_ID2 =  184701;                       //Milk Chiller Relays ID 
+    //const int Channel_ID2 =  184701;                       //Milk Chiller Relays ID 
     //String milkChillerRelays_apiKey ="5CFI74KGNN3Z3CR1";                    //write API key for Channel_ID2(Milk Chiller Relays Section)
 
 
