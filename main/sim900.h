a24df73176e9f82f55085853690ddc55472b3861
Vikas Gaikwad <vksgaikwad3@gmail.com>
2016-12-05 17:41:32 +0530

    httpGET function declaration

--- main/sim900.h
+++ main/sim900.h
@@ -19,7 +19,8 @@ class sim900_GPRS
     void updateThinkSpeak(String channel_apiKey,uint8_t id1,uint8_t id2,uint8_t id3,uint8_t id4,float field1,float field2,float field3,float field4,float field5,float field6,float field7,float field8); //,float field6,float field7,float field8);
     void updateMilkChillerRelays(String channel_apiKey,uint8_t field1,uint8_t field2,uint8_t field3,uint8_t field4,uint8_t field5,uint8_t field6,uint8_t field7,uint8_t field8);
     float hextofloat(uint16_t hByte, uint16_t lByte); 
-    
+    boolean httpGETupdate(String channelKey,float field1,float field2,float field3,float field4,float field5,float field6,float field7,float field8);  
+    void httpInit();
   private:
 
   
